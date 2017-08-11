3b891ba27619a86b027d87877c25be9c080887e0
Andrew Carr <andrew@2metres.com>
2017-08-11 22:22:09 +1000

    ø for the function name bc why not

--- src/components/Altimeter/index.js
+++ src/components/Altimeter/index.js
@@ -2,28 +2,28 @@ import React from 'react'
 
 import styles from './Altimeter.module.css'
 
-const n = (string) =>
+const º = (string) =>
   Math.floor(Math.random() * 4) + string + '|'
 
 const now = new Date()
 const minutes = now.getMinutes() > 9 ? now.getMinutes() : '0' + now.getMinutes()
-const hoursAndMinutes = now.getHours() + ':' + minutes
+const hoursAndMinutes = now.getHours() / 2 + ':' + minutes
 
 const Altimeter = () =>
   <h1 className={ styles.root }>
     {
       '    ' +
       `{${hoursAndMinutes}}` + '§' +
-      n('a') +
-      n('l') +
-      n('t') +
-      n('i') +
-      n('m') +
-      n('e') +
-      n('t') +
-      n('e') +
-      n('r') +
-      '1'
+      º('a') +
+      º('l') +
+      º('t') +
+      º('i') +
+      º('m') +
+      º('e') +
+      º('t') +
+      º('e') +
+      º('r') +
+      Math.floor(Math.random() * 4)
     }<span className={styles.highlight}>[26°c [17°c[</span>
   </h1>
 
